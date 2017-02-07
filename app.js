cd7820fd5241b009d75f25090266556f08087653
Kevin Zavier <kzavier@ucsd.edu>
2017-02-06 16:38:04 -0800

    Inital Express Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+//var port = process.env.PORT || 3000;
+//app.use('/asset', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
