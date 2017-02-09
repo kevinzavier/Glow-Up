245a4a528eac6d729eb01abe9a40ba3c454cb1ca
Kevin Zavier <kzavier@ucsd.edu>
2017-02-08 18:51:37 -0800

    more testing on app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
-//var port = process.env.PORT || 3000;
-//app.use('/asset', express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.send('<html><head><link href=assets/styles.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
+});
+
+app.get('/profile/:id', function (req, res) {
+  res.send('<html><head><link href=assets/styles.css type=text/css rel=stylesheet/></head><body><h1>'+ req.params.id + '</h1></body></html>')
 })
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
